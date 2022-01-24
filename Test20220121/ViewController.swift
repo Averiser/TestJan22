46b769078ee12e16b782a280d89d2fee4b58056b
Averiser <averiser@gmail.com>
2022-01-24 17:50:09 +0300

    second commit - adding to screen programmatically

--- Test20220121/ViewController.swift
+++ Test20220121/ViewController.swift
@@ -9,22 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var redView: UIView!
-  @IBOutlet weak var blueView: UIView!
-  
-  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let view = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
-    view.backgroundColor = .systemRed
-    view.bounds = CGRect(x: -50, y: -50, width: 100, height: 100)
-    self.view.addSubview(redView)
+//    view.backgroundColor = .cyan
     
-    let secondView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-    secondView.backgroundColor = .systemBlue
-    view.addSubview(blueView)
+    
+    
+    
+//let imageView = UIImageView(image: "bear_first")
+//    view.addSubview(imageView)
+    
+    
+//    let view = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+//    view.backgroundColor = .systemRed
+//    view.bounds = CGRect(x: -50, y: -50, width: 100, height: 100)
+//    self.view.addSubview(redView)
+//
+//    let secondView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//    secondView.backgroundColor = .systemBlue
+//    view.addSubview(blueView)
   }
 
 
